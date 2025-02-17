a73327315df633cf5364d07c8c56e4f85c5d1345
SaleenGaha <salingaha@gmail.com>
2025-02-17 20:01:31 +0545

    added qualifications section

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  /
+  
   function validateEmail(email) {
     const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     return regex.test(email);
